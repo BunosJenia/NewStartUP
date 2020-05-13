fef5f8cddca50057ef39e92889507756f9ebb8fb
Evgeny Bunos <evgeny.bunos@wrenkitchens.com>
2020-05-13 17:23:57 +0300

    Implement Idea Generation App

--- NewStartUP/ViewController.swift
+++ NewStartUP/ViewController.swift
@@ -9,12 +9,39 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let ideas = ["Travel Blog", "Apartment Rentals", "Antiques Dealer", "Art Gallery", "Boat Tours", "Coin or Stamp Dealer", "Coffee Bar", "Craft Beer Pub", "Golf Coach", "Ice Cream Shop", "Night Club", "Wine Bar", "Dance Instructor", "House Cleaning", "Soap and Lotion Making", "Car Resale", "Hat Making Business", "Life Coaching"]
+    
+    let persons = ["Rich People", "Cats", "Alpacas", "Students", "Book Lovers", "Zombies", "Swimmers", "Superheroes", "Dragons", "Doctors"]
+    
+    @IBOutlet var answerIdeaLabel: UILabel!
+    @IBOutlet var answerPersorLabel: UILabel!
+    @IBOutlet var shakeButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
+    
+    override func motionEnded(_ motion: UIEvent.EventSubtype, with event: UIEvent?) {
+        guard motion == .motionShake else { return }
 
+        generateStartUp()
+    }
+    
+    @IBAction func shakeButtonTapped(_ sender: Any) {
+        generateStartUp()
+    }
+    
+    private func generateStartUp() {
+        print(ideas.count)
+        print(persons.count)
+        
+        let randomIdea = Int.random(in: 0..<ideas.count)
+        let randomPerson = Int.random(in: 0..<persons.count)
+        
+        answerIdeaLabel.text = ideas[randomIdea]
+        answerPersorLabel.text = persons[randomPerson]
+    }
 
 }
 
---
+++ README.md
@@ -0,0 +1,6 @@
+## App for Idea Generation
+
+Simple Application which can generate more than 150 ideas for your new StartUP.
+Need just to shake your IPhone or click "Generate new Idea" button.
+ 
+ ![Idea Generation](details.png)
